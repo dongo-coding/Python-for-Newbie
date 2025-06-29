68e7d39c4f65a5a098af8440984fd2544ecd3f8a
Do Ngo <bahuynh619@gmail.com>
2025-06-29 23:30:02 +0700

    Update CodeForNewbie00.py

--- CodeForNewbie00.py
+++ CodeForNewbie00.py
@@ -1,4 +1,13 @@
+#01
 name = "Codelearn"
 date_of_birth = 2019
 print("Name: " + name)
 print("Date of birth: " + str(date_of_birth))   //nối một chuỗi và một số bằng str
+
+#02
+length = 7.8
+width = 3.5
+print("Area: " + str(length*width))
+print("Perimeter: " + str(2*(length + width))) // in diện tích và chu vi hình chữ nhật bằng cách nối chuỗi và số bằng str
+
+
